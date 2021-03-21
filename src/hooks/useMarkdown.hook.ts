cb5ff688667ac6953a8dc0ad06bd354986a55546
Julien Calixte <juliencalixte@gmail.com>
2021-03-22 00:14:45 +0100

    :lipstick: (headers) smaller headers

--- src/hooks/useMarkdown.hook.ts
+++ src/hooks/useMarkdown.hook.ts
@@ -2,11 +2,11 @@ import MarkdownIt from 'markdown-it'
 import markdownItClass from '@toycode/markdown-it-class'
 
 const md = new MarkdownIt().use(markdownItClass, {
-  h1: ['title', 'is-1'],
-  h2: ['title', 'is-2'],
-  h3: ['title', 'is-3'],
-  h4: ['title', 'is-4'],
-  h5: ['title', 'is-5'],
+  h1: ['title', 'is-2'],
+  h2: ['title', 'is-3'],
+  h3: ['title', 'is-4'],
+  h4: ['title', 'is-5'],
+  h5: ['title', 'is-6'],
   h6: ['title', 'is-6'],
   table: ['table', 'is-striped', 'is-hoverable', 'is-fullwidth']
 })
