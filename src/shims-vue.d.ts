e8ed90d7828f53e51494d54df91e9240d92c5782
Julien Calixte <julienc@bam.tech>
2022-01-02 13:33:38 +0100

    :heavy_plus_sign: (markdown) add audio markdown support dependency

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,5 @@
 declare module '*.vue' {
-  import { defineComponent } from 'vue'
+  import { value defineComponent } from 'vue'
   const component: ReturnType<typeof defineComponent>
   export default component
 }
@@ -8,3 +8,4 @@ declare module '@toycode/markdown-it-class'
 declare module 'markdown-it-block-embed'
 declare module 'markdown-it-checkbox'
 declare module 'markdown-it-footnote'
+declare module 'markdown-it-html5-media'
