e4bba8246b3679e8ef2c01b71922388217682f08
Julien Calixte <julienc@bam.tech>
2023-07-01 23:08:53 +0200

    🔧 (vite) migrate to vite (#7)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    es2022: true
   },
   extends: [
     'plugin:vue/vue3-essential',
@@ -11,9 +12,6 @@ module.exports = {
     '@vue/prettier/@typescript-eslint',
     'plugin:prettier-vue/recommended'
   ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
