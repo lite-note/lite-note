d067d5cd4972ab24389a2e0f31f3b030812d2d67
Julien Calixte <julienc@bam.tech>
2023-07-01 23:32:13 +0200

    ⬆️ (vue-i18n) add Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { UserConfigExport } from 'vitest/dist/config'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      'node-fetch': 'isomorphic-fetch'
+export default defineConfig(({ command }) => {
+  const config: UserConfigExport = {
+    plugins: [vue(), VueI18nPlugin({})],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        'node-fetch': 'isomorphic-fetch'
+      }
     }
   }
+
+  if (command === 'serve') {
+    config.define = {
+      global: {}
+    }
+  }
+
+  return config
 })
