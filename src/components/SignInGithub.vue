c827aad4b92d82faedc7f7d3f90e97c6bf3bfc4c
Julien Calixte <julienc@bam.tech>
2023-07-02 22:34:06 +0200

    ✨ (github) can log either localhost or lite…

--- src/components/SignInGithub.vue
+++ src/components/SignInGithub.vue
@@ -1,34 +1,26 @@
+<script lang="ts" setup>
+const GITHUB_URL = 'https://github.com/login/oauth/authorize'
+
+const CLIENT_ID = 'Iv1.12dc43d013ce3623'
+const SCOPE = 'repo'
+const REDIRECT_URI = window.location.origin
+
+const url = new URL(GITHUB_URL)
+url.searchParams.set('client_id', CLIENT_ID)
+url.searchParams.set('scope', SCOPE)
+url.searchParams.set('redirect_uri', REDIRECT_URI)
+
+const href = url.toString()
+</script>
+
 <template>
-  <a :href="url" class="sign-in-github button is-primary">
+  <a :href="href" class="sign-in-github button is-primary">
     <span>
       Sign in with
       <img src="@/assets/icons/github.svg" alt="GitHub" />
     </span>
   </a>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-const GITHUB_URL = 'https://github.com/login/oauth/authorize'
-
-const CLIENT_ID = 'Iv1.12dc43d013ce3623'
-
-const SCOPE = 'repo'
-
-export default defineComponent({
-  name: 'SignInGitHub',
-  setup() {
-    const url = new URL(GITHUB_URL)
-    url.searchParams.set('client_id', CLIENT_ID)
-    url.searchParams.set('scope', SCOPE)
-
-    return {
-      url
-    }
-  }
-})
-</script>
 
 <style scoped lang="scss">
 .sign-in-github {
