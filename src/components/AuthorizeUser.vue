68ee2f12e636d5e0617650ef230612792888252a
Julien Calixte <julienc@bam.tech>
2023-07-16 15:32:49 +0200

    ♻️ (authorize user) script with setup

--- src/components/AuthorizeUser.vue
+++ src/components/AuthorizeUser.vue
@@ -1,44 +1,34 @@
+<script lang="ts" setup>
+import { useGitHubLogin } from '@/hooks/useGitHubLogin.hook'
+import { signIn } from '@/modules/user/service/signIn'
+import { onBeforeMount, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+const { saveCredentials } = useGitHubLogin()
+
+const code = route.query.code
+const hasError = ref(false)
+
+onBeforeMount(async () => {
+  if (code) {
+    const token = await signIn(code.toString())
+
+    if ('error' in token) {
+      hasError.value = true
+    } else {
+      token.access_token
+      saveCredentials(token)
+    }
+
+    router.replace({ name: 'Home' })
+  }
+})
+</script>
+
 <template>
   <div class="authorize-user">
     <div v-if="hasError">An error occured when sign in...</div>
   </div>
 </template>
-
-<script lang="ts">
-import { useGitHubLogin } from '@/hooks/useGitHubLogin.hook'
-import { defineComponent, onBeforeMount, ref } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-import { signIn } from '@/modules/user/service/signIn'
-
-export default defineComponent({
-  name: 'AuthorizeUser',
-  setup() {
-    const route = useRoute()
-    const router = useRouter()
-    const { saveCredentials } = useGitHubLogin()
-
-    const code = route.query.code
-    let hasError = ref(false)
-
-    onBeforeMount(async () => {
-      if (code) {
-        const token = await signIn(code.toString())
-
-        if ('error' in token) {
-          hasError.value = true
-        } else {
-          token.access_token
-          saveCredentials(token)
-        }
-
-        router.replace({ name: 'Home' })
-      }
-    })
-
-    return {
-      code,
-      hasError
-    }
-  }
-})
-</script>
