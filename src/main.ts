dfc16722e684eeec5b6eebf6aaa453ea4e895018
Julien Calixte <julienc@bam.tech>
2025-03-01 21:01:39 +0100

    upgrade daisyui and implement new themes

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,27 @@
-import 'notyf/notyf.min.css'
-import './styles/app.css'
+import "notyf/notyf.min.css"
+import "./styles/app.css"
 
-import { createPinia } from 'pinia'
-import { createApp } from 'vue'
-import { createI18n } from 'vue-i18n'
+import { createPinia } from "pinia"
+import { createApp } from "vue"
+import { createI18n } from "vue-i18n"
 
-import { messages } from '@/locales/message'
-import { router } from '@/router/router'
+import { messages } from "@/locales/message"
+import { router } from "@/router/router"
 
-import App from './App.vue'
+import App from "./App.vue"
 
 const i18n = createI18n({
-  locale: 'en',
-  messages
+  locale: "en",
+  messages,
 })
 
-createApp(App).use(router).use(i18n).use(createPinia()).mount('#app')
+createApp(App).use(router).use(i18n).use(createPinia()).mount("#app")
+
+document.documentElement.style.setProperty(
+  "--light-mode",
+  import.meta.env.VITE_LIGHT_MODE,
+)
+document.documentElement.style.setProperty(
+  "--dark-mode",
+  import.meta.env.VITE_DARK_MODE,
+)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,56 +1,59 @@
 /** @type {import('tailwindcss').Config} */
-const dotenv = require('dotenv')
+const dotenv = require("dotenv")
 
 dotenv.config()
 
 const defaultTitleStyles = Array.from(
   { length: 6 },
-  (_, k) => `h${k + 1}`
+  (_, k) => `h${k + 1}`,
 ).reduce(
   (acc, heading) => ({
     ...acc,
     [heading]: {
-      'margin-top': '0',
-      'margin-bottom': '0.5em'
-    }
+      "margin-top": "0",
+      "margin-bottom": "0.5em",
+    },
   }),
-  {}
+  {},
 )
 
 module.exports = {
-  content: ['./src/**/*.{vue,js,ts}'],
+  content: ["./src/**/*.{vue,js,ts}"],
   theme: {
     extend: {
       typography: () => ({
         DEFAULT: {
           css: {
-            'font-size': '13pt',
-            'font-family': '"Courier Prime", monospace',
+            "font-size": "13pt",
+            "font-family": '"Courier Prime", monospace',
             ...defaultTitleStyles,
             p: {
-              'margin-top': '0.8em',
-              'margin-bottom': '0.8em',
-              'text-align': 'justify'
+              "margin-top": "0.8em",
+              "margin-bottom": "0.8em",
+              "text-align": "justify",
             },
             img: {
-              'margin-top': 0,
-              'margin-bottom': 0,
-              'border-radius': '1rem'
+              "margin-top": 0,
+              "margin-bottom": 0,
+              "border-radius": "1rem",
             },
             a: {
-              'text-decoration': 'none',
-              color: 'oklch(var(--s))'
+              "text-decoration": "none",
+              color: "var(--color-primary)",
             },
-            'a:hover': {
-              'text-decoration': 'underline'
+            "a.btn-primary": {
+              color: "var(--color-secondary-content)",
+            },
+            "a:hover": {
+              "text-decoration": "underline",
             },
             li: {
-              'margin-top': 0,
-              'margin-bottom': 0
-            }
-          }
-        }
-      })
-    }
-  }
+              "margin-top": 0,
+              "margin-bottom": 0,
+            },
+          },
+        },
+      }),
+    },
+  },
 }
