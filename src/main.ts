0212e963fdc2510afa0c7c376509f0726e4abeb2
Julien Calixte <julienc@bam.tech>
2025-03-03 15:29:20 +0100

    feat lint main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-import "notyf/notyf.min.css"
-import "./styles/app.css"
+import 'notyf/notyf.min.css'
+import './styles/app.css'
 
-import { createPinia } from "pinia"
-import { createApp } from "vue"
-import { createI18n } from "vue-i18n"
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 
-import { messages } from "@/locales/message"
-import { router } from "@/router/router"
+import { messages } from '@/locales/message'
+import { router } from '@/router/router'
 
-import App from "./App.vue"
+import App from './App.vue'
 
 const i18n = createI18n({
-  locale: "en",
-  messages,
+  locale: 'en',
+  messages
 })
 
-createApp(App).use(router).use(i18n).use(createPinia()).mount("#app")
+createApp(App).use(router).use(i18n).use(createPinia()).mount('#app')
