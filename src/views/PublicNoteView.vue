9d48329a30b596e6d8276a4cdf74f72e94259f1c
Julien Calixte <julienc@bam.tech>
2026-02-10 20:24:04 +0100

    feat: get aka too

--- src/views/PublicNoteView.vue
+++ src/views/PublicNoteView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
+import { getAka } from "@/modules/atproto/getAka"
 import { PublicNoteListItem } from "@/modules/note/models/Note"
-import { useAsyncState } from "@vueuse/core"
+import { computedAsync, useAsyncState } from "@vueuse/core"
 
 const { state, isLoading } = useAsyncState<{
   notes: PublicNoteListItem[]
@@ -11,6 +12,15 @@ const { state, isLoading } = useAsyncState<{
   },
   { notes: [] },
 )
+
+const aka = computedAsync<Map<string, string>>(async () => {
+  if (state.value.notes.length === 0) {
+    return new Map()
+  }
+  return getAka(new Set(state.value.notes.map((n) => n.did)))
+}, new Map())
+
+const getAlias = (did: string) => aka.value.get(did) ?? ""
 </script>
 
 <template>
@@ -18,7 +28,7 @@ const { state, isLoading } = useAsyncState<{
   <div class="public-note-view" v-else>
     <ul>
       <li v-for="note in state.notes">
-        {{ note.did }}/{{ note.rkey }}: {{ note.title }}
+        {{ getAlias(note.did) }}: {{ note.title }}
       </li>
     </ul>
   </div>
